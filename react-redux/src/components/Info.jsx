4e2da3f78c49cdaf2e83e34ce4c8d8ecd9eef51f
mfaydin49 <mfaydin49@gmail.com>
2023-07-07 14:13:31 +0200

    fixed route info in info component

--- react-redux/src/components/Info.jsx
+++ react-redux/src/components/Info.jsx
@@ -1,4 +1,4 @@
-import {useLocation, useNavigate, useNavigation} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 
 const Info = () => {
     let location = useLocation();
@@ -6,8 +6,10 @@ const Info = () => {
     return (
         <div className="info">
             <div className="info-container">
-                <div
-                    className="info-navigation">{location.pathname === "/" ? "Home Page > e-Learning Courses" : `Home Page > e-Learning Courses ${location.pathname}`}</div>
+                <div className="info-navigation">{location.pathname === "/"
+                    ? "Home Page > e-Learning Courses"
+                    : `Home Page > e-Learning Courses > ${location.pathname.slice(1)}`}
+                </div>
                 <div className="info-user">
                     <span>admin</span>
                 </div>
